559a9ba10cfde00972aaabe768519702af2b11d4
osamamukhtar11 <osamamukhtar11@gmail.com>
2019-05-22 17:05:07 +0300

    Added barplot methods

--- visualizations.py
+++ visualizations.py
@@ -82,7 +82,7 @@ def histogram(data, bins, cumulative=False, x_label = "Value", y_label = "Freque
     else:
         plt.show()
 
-def overlaid_histogram(data1, data2, bins = 0, data1_name="", data1_color="#539caf", data2_name="", data2_color="#7663b0", x_label="Value", y_label="Frequency", title="Overlaid Histogram", saveOrShow=False, filename='overlaid_histogram'):
+def overlaid_histogram(data1, data2, bins = 0, data1_name="X1", data1_color="#539caf", data2_name="X2", data2_color="#7663b0", x_label="Value", y_label="Frequency", title="Overlaid Histogram", saveOrShow=False, filename='overlaid_histogram'):
     # Overlay 2 histograms to compare them
     # Set the bounds for the bins so that the two distributions are fairly compared
     '''
@@ -121,3 +121,97 @@ def overlaid_histogram(data1, data2, bins = 0, data1_name="", data1_color="#539c
         plt.savefig(filename+'.png')
     else:
         plt.show()
+
+def barplot(x_data, y_data, error_data, x_label="", y_label="", title="Barplot", saveOrShow=False, filename='barplot'):
+    '''
+    :param x_data: variable on x-axis, may or may not be categorical
+    :param y_data: variable on y-axis, must be numeric
+    :param error_data: Error bars give a general idea of how precise a measurement is, or conversely, how far from the reported value the true (error free) value might be. If the value displayed on your barplot is the result of an aggregation (like the mean value of several data points), you may want to display error bars.
+    :param x_label: Label for x-axis
+    :param y_label: label for y-axis
+    :param title: title for the diagram
+    :param saveOrShow: if true, save the plot, else show the plot
+    :param filename: if saveOrShow is True, plot is saved as a PNG file with the filename. Default value is 'overlaid_histogram.png'
+    :return:
+    '''
+    _, ax = plt.subplots()
+    # Draw bars, position them in the center of the tick mark on the x-axis
+    ax.bar(x_data, y_data, color = '#539caf', align = 'center')
+    # Draw error bars to show standard deviation, set ls to 'none'
+    # to remove line between points
+    ax.errorbar(x_data, y_data, yerr = error_data, color = '#297083', ls = 'none', lw = 2, capthick = 2)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    if saveOrShow:
+        plt.savefig(filename+'.png')
+    else:
+        plt.show()
+
+def groupedbarplot(x_data, y_data_list, colors, y_data_names, x_label="", y_label="", title="Grouped Barplot", saveOrShow=False, filename='grouped_barplot'):
+    '''
+    :param x_data: variable on x-axis, may or may not be categorical
+    :param y_data_list: list of lists where each list contains values for different groups e.g. list1 consists of scores above 90 for men and women
+    :param colors: list of colors for each group
+    :param y_data_names: names to be displayed for each group
+    :param x_label: label on x-axis
+    :param y_label: label on y-axis
+    :param title: title of the diagram
+    :param saveOrShow: if true, save the plot, else show the plot
+    :param filename: if saveOrShow is True, plot is saved as a PNG file with the filename. Default value is 'overlaid_histogram.png'
+    :return:
+    '''
+    _, ax = plt.subplots()
+    # Total width for all bars at one x location
+    total_width = 0.8
+    # Width of each individual bar
+    ind_width = total_width / len(y_data_list)
+    # This centers each cluster of bars about the x tick mark
+    alteration = np.arange(-(total_width/2), total_width/2, ind_width)
+
+    # Draw bars, one category at a time
+    for i in range(0, len(y_data_list)):
+        # Move the bar to the right on the x-axis so it doesn't
+        # overlap with previously drawn ones
+        ax.bar(x_data + alteration[i], y_data_list[i], color = colors[i], label = y_data_names[i], width = ind_width)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    ax.legend(loc = 'upper right')
+    if saveOrShow:
+        plt.savefig(filename+'.png')
+    else:
+        plt.show()
+
+def stackedbarplot(x_data, y_data_list, colors, y_data_names, x_label="", y_label="", title="Stacked Barplot", saveOrShow=False, filename='stacked_barplot'):
+    '''
+    :param x_data: variable on x-axis, may or may not be categorical
+    :param y_data_list: list of lists where each list contains values for different groups e.g. list1 consists of scores above 90 for men and women
+    :param colors: list of colors for each group
+    :param y_data_names: names to be displayed for each group
+    :param x_label: label on x-axis
+    :param y_label: label on y-axis
+    :param title: title of the diagram
+    :param saveOrShow: if true, save the plot, else show the plot
+    :param filename: if saveOrShow is True, plot is saved as a PNG file with the filename. Default value is 'overlaid_histogram.png'
+    :return:
+    '''
+    _, ax = plt.subplots()
+    # Draw bars, one category at a time
+    for i in range(0, len(y_data_list)):
+        if i == 0:
+            ax.bar(x_data, y_data_list[i], color = colors[i], align = 'center', label = y_data_names[i])
+        else:
+            # For each category after the first, the bottom of the
+            # bar will be the top of the last category
+            ax.bar(x_data, y_data_list[i], color = colors[i], bottom = y_data_list[i - 1], align = 'center', label = y_data_names[i])
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    ax.legend(loc = 'upper right')
+    if saveOrShow:
+        plt.savefig(filename+'.png')
+    else:
+        plt.show()
+
+# code inspired from https://towardsdatascience.com/5-quick-and-easy-data-visualizations-in-python-with-code-a2284bae952f
