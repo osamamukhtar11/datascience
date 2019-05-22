ca421eec940d8a4ea8dd6ab810a54049d30b255e
osamamukhtar11 <osamamukhtar11@gmail.com>
2019-05-22 14:28:25 +0300

    Added lineplot method

--- visualizations.py
+++ visualizations.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def scatterplot(x_data, y_data, x_label="", y_label="", title="", color = "r", yscale_log=False):
-
     # Create the plot object
     _, ax = plt.subplots()
 
@@ -12,6 +11,19 @@ def scatterplot(x_data, y_data, x_label="", y_label="", title="", color = "r", y
 
     if yscale_log == True:
         ax.set_yscale('log')
+
+    # Label the axes and provide a title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+def lineplot(x_data, y_data, x_label="", y_label="", title=""):
+    # Create the plot object
+    _, ax = plt.subplots()
+
+    # Plot the best fit line, set the linewidth (lw), color and
+    # transparency (alpha) of the line
+    ax.plot(x_data, y_data, lw = 2, color = '#539caf', alpha = 1)
 
     # Label the axes and provide a title
     ax.set_title(title)
