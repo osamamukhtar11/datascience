e3e4e07a41115bec1e5a704de52fdc9079a3b8cf
osamamukhtar11 <osamamukhtar11@gmail.com>
2019-07-25 17:55:49 +0300

    Added code for visualizing any 3D dataframe with labels (PCA as an example) as a GIF

--- visualizations.py
+++ visualizations.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import imageio
+import os
+import pandas as pd
 
 def scatterplot(x_data, y_data, x_label="X", y_label="Y", title="Scatter plot", color = "r", yscale_log=False, saveOrShow=False, filename='scatter'):
     '''
@@ -237,4 +239,59 @@ def scatterplot_3d(x, y, z, labels):
     ax.set_zlabel('d-3')
     plt.savefig('scatterplot-3d.jpg')
 
-# code inspired from https://towardsdatascience.com/5-quick-and-easy-data-visualizations-in-python-with-code-a2284bae952f
+def make_gif(df, starting_angle = 70, ending_angle = 210, step_size = 2, gif_parts_folder = 'gifParts', filename_sample = 'PCA_angle', gif_name = 'output.gif'):
+    '''
+    Create a GIF by creating multiple .PNG 3d plots each showing a different angle of graph
+    :param df: dataframe containing three PCA components with names 'pca-one', 'pca-two', and 'pca-three', and 'y' as output label
+    :param starting_angle: Angle from which the gif should start, and first picture should be shown, recommendation is 70 degrees
+    :param ending_angle: Angle where gif should end, and the last picture be shown, recommendation is 210
+    :param step_size: difference between angles of two pictures
+    :param gif_parts_folder: directory where all .png files are saved and then used as input for creating .gif
+    :param filename_sample: prefix of file name for each part used for gif, angle is appended to this sample name.
+    :param gif_name: name of output GIF file
+    :return:
+    '''
+    df['output_values'] = pd.Categorical(df['y'])
+    my_color = df['output_values'].cat.codes
+    for angle in range(starting_angle, ending_angle, step_size):
+        # Plot initialisation
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(df['pca-one'], df['pca-two'], df['pca-three'], c=my_color, cmap="Set2_r", s=60)
+
+        # make simple, bare axis lines through space:
+        xAxisLine = ((min(df['pca-one']), max(df['pca-one'])), (0, 0), (0, 0))
+        ax.plot(xAxisLine[0], xAxisLine[1], xAxisLine[2], 'r')
+        yAxisLine = ((0, 0), (min(df['pca-two']), max(df['pca-two'])), (0, 0))
+        ax.plot(yAxisLine[0], yAxisLine[1], yAxisLine[2], 'r')
+        zAxisLine = ((0, 0), (0, 0), (min(df['pca-three']), max(df['pca-three'])))
+        ax.plot(zAxisLine[0], zAxisLine[1], zAxisLine[2], 'r')
+
+        ax.view_init(30, angle)
+
+        # label the axes
+        ax.set_xlabel("PC1")
+        ax.set_ylabel("PC2")
+        ax.set_zlabel("PC3")
+        ax.set_title("PCA MNIST Data")
+        if not os.path.exists('gifParts'):
+            os.mkdir(gif_parts_folder)
+        filename = gif_parts_folder+ '/' + filename_sample + str(angle) + '.png'
+        plt.savefig(filename, dpi=96)
+
+        episode_frames = []
+        time_per_step = 0.25
+        for root, _, files in os.walk(gif_parts_folder):
+            file_paths = [os.path.join(root, file) for file in files]
+            # sorted by modified time
+            file_paths = sorted(file_paths, key=lambda x: os.path.getmtime(x))
+            episode_frames = [imageio.imread(file_path)
+                              for file_path in file_paths if file_path.endswith('.png')]
+        episode_frames = np.array(episode_frames)
+        imageio.mimsave(gif_name, episode_frames, duration=time_per_step)
+
+'''
+References:
+    https://towardsdatascience.com/5-quick-and-easy-data-visualizations-in-python-with-code-a2284bae952f
+    https://towardsdatascience.com/interactive-data-visualization-167ae26016e8
+'''
